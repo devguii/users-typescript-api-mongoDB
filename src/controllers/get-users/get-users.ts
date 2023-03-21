8bb4d4901ad225b7e256ae2318f9b3a9704b68c1
gui <guigon@alunos.utfpr.edu.br>
2023-03-21 18:16:59 -0300

    feat: add GetUsersController

---
+++ src/controllers/get-users/get-users.ts
@@ -0,0 +1,23 @@
+import { IGetUsersController, IGetUsersRepository } from "./protocols";
+
+export class GetUsersController implements IGetUsersController {
+  constructor(private readonly getUsersRepository: IGetUsersRepository) {}
+
+  async handle() {
+    try {
+      // validar a requisição
+      // direcionar a chamada para o Repository
+      const users = await this.getUsersRepository.getUsers();
+
+      return {
+        statusCode: 200,
+        body: users,
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: "Something went wrong.",
+      };
+    }
+  }
+}
