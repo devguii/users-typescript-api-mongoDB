947cc21817df678ce351957e6980f61f434ef2f3
gui <guigon@alunos.utfpr.edu.br>
2023-04-09 20:32:31 -0300

    feat: add MongoClient with mongodb connection

---
+++ src/database/mongo.ts
@@ -0,0 +1,21 @@
+import { MongoClient as Mongo, Db } from "mongodb";
+
+export const MongoClient = {
+  client: undefined as unknown as Mongo,
+  db: undefined as unknown as Db,
+
+  async connect(): Promise<void> {
+    const url =
+      process.env.MONGODB_URL || "mongodb+srv://cluster0.77m4ihk.mongodb.net";
+    const username = process.env.MONGODB_USERNAME;
+    const password = process.env.MONGODB_PASSWORD;
+
+    const client = new Mongo(url, { auth: { username, password } });
+    const db = client.db("users-db");
+
+    this.client = client;
+    this.db = db;
+
+    console.log("connected to MongoDB!");
+  },
+};
