12fba33851a7ad2c6136ee5537e2f318ae214ee5
gui <guigon@alunos.utfpr.edu.br>
2023-04-15 13:00:39 -0300

    feat: ensure GetUsersMongoRepository gets users from mongo

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ const main = async () => {
     const mongoGetUsersRepository = new MongoGetUsersRepository();
 
     const getUsersController = new GetUsersController(mongoGetUsersRepository);
+
     const { body, statusCode } = await getUsersController.handle();
 
     res.send(body).status(statusCode);
