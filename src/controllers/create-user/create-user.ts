2f205fb49323e765e5a380de471150faaed5570c
gui <guigon@alunos.utfpr.edu.br>
2023-04-16 23:04:23 -0300

    feat: add CreateUserController

---
+++ src/controllers/create-user/create-user.ts
@@ -0,0 +1,38 @@
+import { User } from "../../models/user";
+import { HttpRequest, HttpResponse } from "../protocols";
+import {
+  CreateUserParams,
+  ICreateUserController,
+  ICreateUserRepository,
+} from "./protocols";
+
+export class CreateUserController implements ICreateUserController {
+  constructor(private readonly createUserRepository: ICreateUserRepository) {}
+
+  async handle(
+    httpRequest: HttpRequest<CreateUserParams>
+  ): Promise<HttpResponse<User>> {
+    try {
+      //validar se body existe
+
+      if (!httpRequest.body) {
+        return {
+          statusCode: 400,
+          body: "Please specify a body",
+        };
+      }
+
+      const user = await this.createUserRepository.createUser(httpRequest.body);
+
+      return {
+        statusCode: 201,
+        body: user,
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: "Something went wrong.",
+      };
+    }
+  }
+}
